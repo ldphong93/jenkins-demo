bf8a518277c65cd1a0135dd998bab85e4ddb82b3
phong <phong_le@epam.com>
2022-01-29 11:10:10 +0700

    modified to phong's theme park config

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('theme-park-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/ldphong93/JenkinsSample.git'
+                    }
+                    branch 'main'
+                }
+            }
         }
     }
 }
